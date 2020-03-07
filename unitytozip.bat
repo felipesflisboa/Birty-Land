cbc35d15c03b7b1e90689b3d515712fe43c33f88
felipesflisboa <felipesflisboa@users.noreply.github.com>
2020-03-07 10:11:07 -0300

    unittozip ignore some folders

--- unitytozip.bat
+++ unitytozip.bat
@@ -2,5 +2,5 @@
 ECHO Enter a the file name, including extension. Valid extensions are: zip/7z
 SET /P param=
 @echo on
-7z a %param% * -r -x!Library -x!obj -x!Unused
+7z a %param% * -r -x!Library -x!obj -x!Temp -x!.vs -x!Unused
 PAUSE
